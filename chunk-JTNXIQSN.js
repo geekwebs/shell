bee329d34d0186f7b4210cf13985394d2070e6f4
Kusnendi Muhamad <kusnendi.muhamad@ad-ins.com>
2024-07-09 10:26:54 +0700

    Updates

---
+++ chunk-JTNXIQSN.js
@@ -0,0 +1,1 @@
+import{a}from"./chunk-IQVSBQ3X.js";import"./chunk-7G2FUV74.js";import"./chunk-ZTVWNM4H.js";import"./chunk-GEQ6AVHT.js";export{a as MyIpComponent};
---
+++ chunk-UGCXFW2B.js
@@ -0,0 +1,1 @@
+import{Ma as i,Na as a,Sa as r,Z as o,Za as s}from"./chunk-GEQ6AVHT.js";var f=(()=>{let e=class e{};e.\u0275fac=function(n){return new(n||e)},e.\u0275cmp=o({type:e,selectors:[["app-forbidden"]],standalone:!0,features:[s],decls:2,vars:0,template:function(n,c){n&1&&(i(0,"p"),r(1,"Forbidden access!"),a())}});let t=e;return t})();export{f as ForbiddenComponent};
